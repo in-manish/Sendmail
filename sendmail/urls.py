83339d2b1d0497c2d2a25c5bf8af977018ab5e6a
Manish <memanishindian@gmail.com>
2018-07-09 17:49:50 +0530

    added django login page(built in) partial functioning

--- sendmail/urls.py
+++ sendmail/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
 from . import views
 
 urlpatterns=[
 	path('send', views.composemail, name='composemail'),
-	path('', views.home, name='home')
+	path('', views.home, name='home'),
+	path('login', auth_views.login, name='login'),
+
+
 ]
